e389a092ea8a6f475cf6f676dde8b80dbbd78213
Gardener <kotov.irk@gmail.com>
2018-10-19 13:54:29 +0800

    page 60

---
+++ app/src/main/java/ru/geekbase/gardener/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package ru.geekbase.gardener.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int TextResId, boolean answerTrue){
+
+        mAnswerTrue = answerTrue;
+        mTextResId = TextResId;
+    }
+}
