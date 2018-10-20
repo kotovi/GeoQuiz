0932c6e5ef033181ebdb7ebab39394856c4344c2
i.kotov_home <gardener@iMac-Gardener.local>
2018-10-20 18:00:24 +0800

    page 66
    подключена кнопка. До инкапсуляции метода с выбором вопроса

--- app/src/main/java/ru/geekbase/gardener/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/geekbase/gardener/geoquiz/QuizActivity.java
@@ -29,6 +29,10 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextViev = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextViev.setText(question);
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +53,16 @@ public class QuizActivity extends AppCompatActivity {
                 toast.setGravity(Gravity.TOP,0,0);
                 toast.show();
 
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextViev.setText(question);
             }
         });
     }
