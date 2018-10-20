9e367cb1371aa5c0a4dafb3084b5850f281ff636
i.kotov_home <gardener@iMac-Gardener.local>
2018-10-20 18:31:45 +0800

    page 70

--- app/src/main/java/ru/geekbase/gardener/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/geekbase/gardener/geoquiz/QuizActivity.java
@@ -22,6 +22,7 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_russia, true)
     };
+
     private int mCurrentIndex = 0;
 
     @Override
@@ -30,28 +31,18 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextViev = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextViev.setText(question);
-
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
-                //описание действия
+                checkAnswer(true);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast toast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP,0,0);
-                toast.show();
+                checkAnswer(false);
 
             }
         });
@@ -61,9 +52,25 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getTextResId();
-                mQuestionTextViev.setText(question);
+                updateQuestion();
             }
         });
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextViev.setText(question);
+    }
+
+    private void checkAnswer (boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
